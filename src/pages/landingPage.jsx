a69715041bd04aaf26a1ef76b9773e6573c9f564
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-03 18:19:35 +0530

    first push scam alert

---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import reactDOM from 'reactDOM';
+
+
+const Landing = () => {
+    return(
+        <div>
+        <div>
+            <hi>hi</hi>
+        </div>
+        </div>
+    )
+}
+export default Landing;
