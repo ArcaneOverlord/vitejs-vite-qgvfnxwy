aa591528330037d4c4559cb298537209c27b7120
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 17:30:20 +0530

    update

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -4,9 +4,15 @@ import React from 'react';
 const LandingHead = () => {
   return (
     <div>
+      <div>
       <img src='/image/shield.svg' />
+      </div>
+      <div>
+        <img src='/image/gearb.svg'/>
+      </div>
       
     </div>
+    
   )
 };
 export default LandingHead;
