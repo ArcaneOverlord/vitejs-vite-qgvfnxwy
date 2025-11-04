8959d65156f4ae45d775b6886d7b3bd927b1b425
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 16:41:58 +0530

    update landingPageHead.jsx

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LandingHead = () => {
   return (
     <div>
-      <img src="/public/image/sheild.svg"/>
+      <img src='/image/sheild.svg' />
       
     </div>
   )
