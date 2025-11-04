3a193932d3e66b18a4c0a95ab4fb9cc579af7ddd
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 11:39:46 +0530

    color test

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -4,7 +4,7 @@ const Landing = () => {
     return (
         <div className="bg-red-500 h-screen w-screen flex items-center justify-center">
             <div>
-                <h1 className="text-red-500 text-2xl">hi</h1>
+                <h1 className="text-blue-500 text-2xl">hi</h1>
             </div>
         </div>
     );
