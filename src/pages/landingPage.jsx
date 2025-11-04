a5c672ce34d135a4c2f6142387731a72b03d2490
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 17:58:55 +0530

    update

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import LandingHead from '../parts/../parts/landingPageHead.jsx';
+import LandingBody from '../../parts/landingPageBody.jsx';
+import LandingFooter from '../../parts/landingPageFooter.jsx';
 
 const Landing = () => {
     return (
-        <div className="bg-[#e7e7e9] h-screen w-screen flex items-center justify-center">
-            <div className=" ">
+        <div className="bg-[#e7e7e9] h-screen w-screen flex items-center justify-center gap-[5px]">
+            <div>
                 <LandingHead/>
+            </div>
+            <div>
+                
             </div>
         </div>
     );
