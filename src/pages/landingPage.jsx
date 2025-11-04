2577550961379501b71300db15f3ac5c62a78641
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 11:33:52 +0530

    update landingPage.jsx

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
-
 const Landing = () => {
-    return(
-        <div className="bg-black h-screen w-screen">
-        <div>
-            <hi className="text-red font-2xl">hi</hi>
+    return (
+        <div className="bg-black h-screen w-screen flex items-center justify-center">
+            <div>
+                <h1 className="text-red-500 text-2xl">hi</h1>
+            </div>
         </div>
-        </div>
-    )
-}
+    );
+};
+
 export default Landing;
