15054ca2ffb8e2f145de05bb97d87611a1b6fe39
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 15:16:01 +0530

    landing head

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LandingHead from '../parts/../parts/landingPageHead.jsx';
 
 const Landing = () => {
     return (
-        <div className="bg-red-500 h-screen w-screen flex items-center justify-center">
+        <div className="bg-white h-screen w-screen flex items-center justify-center">
             <div>
-                <h1 className="text-blue-500 text-2xl">hi</h1>
+                <LandingHead/>
             </div>
         </div>
     );
---
+++ src/parts/landingPageHead.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const LandingHead = () => {
+  return (
+    <div>
+      <img src="/src/assets/sheild.svg"></img>
+    </div>
+  )
+};
+export default LandingHead;
