4f7108569d33a5506c229cb2279d2ba7014ff074
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 16:43:38 +0530

    update landingPage.jsx

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -3,7 +3,7 @@ import LandingHead from '../parts/../parts/landingPageHead.jsx';
 
 const Landing = () => {
     return (
-        <div className="bg-white h-screen w-screen flex items-center justify-center">
+        <div className="bg-black h-screen w-screen flex items-center justify-center">
             <div>
                 <LandingHead/>
             </div>
