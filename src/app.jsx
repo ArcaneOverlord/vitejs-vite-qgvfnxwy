f3cd1f7d41bdd68689ad28c9703d1af989b778ae
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 07:44:50 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Landing from './pages/landingPage.jsx';
 
-function App (){
-  const router= createBrowserRouter([
+function App() {
+  const router = createBrowserRouter([
     {
-      path:"/",
-      element:<Landing/>
-    }
+      path: '/',
+      element: <Landing />,
+    },
   ]);
 
-return(
-  <div>
-  <RouterProvider router={router}/>
-  </div>
-)
-};
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
 
 export default App;
