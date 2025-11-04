ff6c62b5ebda4b5fb08950f9a92304264d3c9067
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-04 11:36:47 +0530

    update landingPage.jsx

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div className="bg-black h-screen w-screen flex items-center justify-center">
+        <div className="bg-red-500 h-screen w-screen flex items-center justify-center">
             <div>
                 <h1 className="text-red-500 text-2xl">hi</h1>
             </div>
