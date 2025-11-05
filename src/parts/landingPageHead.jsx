0e7fb135a00f8dcc9e5ef6ddd241089d070eb795
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 11:12:38 +0530

    update landingPageHead.jsx

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -5,11 +5,11 @@ const LandingHead = () => {
   return (
     <div className="h-full w-screen flex flex-row" >
       <div className= "flex flex-[1]  items-center justify-start h-full">
-      <img src='/image/shield.svg' className="object-contain object-center " />
+      <img src='/image/shield.svg' className="object-contain object-center max-h-[40px]" />
       </div>
       <div className= "flex flex-[1] items-center justify-end h-full">
         <img src='/image/gearb.svg'
-        className="object-contain object-center"/>
+        className="object-contain object-center max-h-[40px]"/>
       </div>
     </div>
     
