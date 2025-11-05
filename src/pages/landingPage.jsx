10d6e91960d9f356fc4ff46b2e8cfef548666b0d
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 10:41:45 +0530

    update

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -5,7 +5,7 @@ import LandingFooter from '../parts/landingPageFooter.jsx';
 
 const Landing = () => {
     return (
-        <div className="bg-[#e7e7e9] max-h-screen max-w-screen flex flex-col items-center justify-center gap-[5px]
+        <div className="bg-[#e7e7e9] h-screen w-screen flex flex-col items-center justify-center gap-[5px]
         overflow-hidden">
         <div className="bg-[#ffffff] w-screen flex flex-[1] h-full">
           <LandingHead/>
