0abaea11f610c4482d413e628c9d8229d665f73c
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 11:01:46 +0530

    update landingPageHead.jsx

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const LandingHead = () => {
   return (
     <div className="h-full w-screen flex flex-row" >
-      <div className= "flex flex-[1] p-3 justify-center">
-      <img src='/image/shield.svg' className="object-contain object-center max-h-[20px]" />
+      <div className= "flex flex-[1] p-3 items-center flex-start">
+      <img src='/image/shield.svg' className="object-contain object-center max-h-[30px]" />
       </div>
-      <div className= "flex flex-[1] p-3 justify-center">
+      <div className= "flex flex-[1] p-3 items-center flex-end">
         <img src='/image/gearb.svg'
-        className="object-contain object-center max-h-[20px]"/>
+        className="object-contain object-center max-h-[30px]"/>
       </div>
     </div>
     
