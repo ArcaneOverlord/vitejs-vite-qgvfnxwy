54507215e89daf0248152f0f46907e5f3b43d3dc
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 10:03:29 +0530

    update landingPageFooter.jsx

--- src/parts/landingPageFooter.jsx
+++ src/parts/landingPageFooter.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const LandingFooter = () => {
   return (
-  <div className="bg-[#ffffff] w-screen flex-[1] max-h-[100px] ">
-    <div>
+  <div className="bg-[#ffffff] w-screen flex-[1] max-h-[100px] flex flex-col ">
+    <div className = "flex-[1] flex">
       <img src= "./image/house.svg" />
     </div>
-    <div>
+    <div className = "flex-[1] flex">
       <img src= "./image/house.svg" />
     </div>
-    <div>
+    <div className = "flex-[1] flex">
       <img src= "./image/house.svg" />
     </div>
   </div>
