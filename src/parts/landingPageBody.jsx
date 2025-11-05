17ad20dfd5a5723bf87d7e8f1ce222ac1e6a0770
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 12:17:43 +0530

    update landingPageBody.jsx

--- src/parts/landingPageBody.jsx
+++ src/parts/landingPageBody.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const LandingBody = () => {
   return (
-    <div className= "h-full w-screen justify-center items-center bg-red-500 flex ">
-      <h1 className= "fond-bold">Your Safety, Our Priority</h1>
-      <p>Help protect the community by Reporting scams quickly and easily</p>
+    <div className= "h-full w-screen justify-center items-center flex flex-col ">
+      <h1 className= "font-bold text-x1 text-black">Your Safety, Our Priority</h1>
+      <p className="text-sm text-[#7c7d81]">Help protect the community by Reporting scams quickly and easily</p>
       
       
     </div>
