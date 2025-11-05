6f921a286b8e41bfb3657cdcd5a94e3d653634f9
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 17:13:04 +0530

    update

--- src/parts/landingPageFooter.jsx
+++ src/parts/landingPageFooter.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const LandingFooter = () => {
   return (
   <div className="bg-[#ffffff] w-screen h-full flex flex-row ">
-    <div className = "flex-[1] flex p-1">
+    <div className = "flex-[1] flex justify-center items-center">
       <img src= "./image/house.svg" className="object-contain object-center max-h-[30px]" />
     </div>
-    <div className = "flex-[1] flex">
+    <div className = "flex-[1] flex justify-center items-center">
       <img src= "./image/shield icon.svg" 
       className="object-contain object-center max-h-[30px]" />
     </div>
-    <div className = "flex-[1] flex">
+    <div className = "flex-[1] flex justify-center items-center">
       <img src= "./image/Bell.svg"
       className="object-contain object-center max-h-[30px]" />
     </div>
