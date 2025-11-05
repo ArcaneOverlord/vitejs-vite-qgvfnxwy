64e3e8981608590b6f7a78f0520a4a7b1708abbd
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 10:14:40 +0530

    update landingPageHead.jsx

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const LandingHead = () => {
   return (
-    <div className="bg-[#ffffff] w-screen flex flex-[1] " >
+    <div className="bg-[#ffffff] w-screen flex flex-[1] p-10 gap-7 " >
       <div className= "flex flex-[1] p-3 ">
       <img src='/image/shield.svg' className="h-[100%]" />
       </div>
