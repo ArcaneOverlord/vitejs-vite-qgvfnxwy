397b9a1de22bffa56eebb9f0df37d068c3aa3744
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 11:02:54 +0530

    update landingPageFooter.jsx

--- src/parts/landingPageFooter.jsx
+++ src/parts/landingPageFooter.jsx
@@ -4,15 +4,15 @@ const LandingFooter = () => {
   return (
   <div className="bg-[#ffffff] w-screen h-full flex flex-row ">
     <div className = "flex-[1] flex p-1">
-      <img src= "./image/house.svg" className="object-contain object-center" />
+      <img src= "./image/house.svg" className="object-contain object-center max-h-[30px]" />
     </div>
     <div className = "flex-[1] flex">
       <img src= "./image/house.svg" 
-      className="object-contain object-center" />
+      className="object-contain object-center max-h-[30px]" />
     </div>
     <div className = "flex-[1] flex">
       <img src= "./image/house.svg"
-      className="object-contain object-center" />
+      className="object-contain object-center max-h-[30px]" />
     </div>
   </div>
   )
