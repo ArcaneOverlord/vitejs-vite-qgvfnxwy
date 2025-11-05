dff2cf36bc906d99085c969b795255c8762109a7
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-05 09:54:37 +0530

    update landingPageHead.jsx

--- src/parts/landingPageHead.jsx
+++ src/parts/landingPageHead.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 const LandingHead = () => {
   return (
     <div className="bg-[#ffffff] w-screen flex-[1] " >
-      <div className="h-[100%] flex flex-start">
+      <div>
       <img src='/image/shield.svg' />
       </div>
-      <div className="h-[100%] flex flex-end">
-        <img src='/image/gearb.svg'
-        className= "h-[100%]"/>
+      <div>
+        <img src='/image/gearb.svg'/>
       </div>
     </div>
     
