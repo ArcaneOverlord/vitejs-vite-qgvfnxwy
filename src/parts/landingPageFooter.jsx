93f99ceb5b56bbcf506baed6a6e81096cf799046
ArcaneOverlord <akshaybabu1257@gmail.com>
2025-11-06 09:51:13 +0530

    update

--- src/parts/landingPageFooter.jsx
+++ src/parts/landingPageFooter.jsx
@@ -13,7 +13,7 @@ const LandingFooter = () => {
             <p className="text-sm text-[#7c7d81]">Protection</p>
     </div>
     <div className = "flex-[1] flex flex-col justify-center items-center">
-      <img src= "./image/Bell.svg"
+      <img src= "./image/Bell grey.svg"
       className="object-contain object-center max-h-[30px]" />
             <p className="text-sm text-[#7c7d81]">Alerts</p>
     </div>
